4f66038c1fd3a5cab95b446187e040819c7c7e9d
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-21 00:02:53 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi cara pengguna boolean
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int umur = 19;
+	    boolean dewasa;
+	    dewasa = (umur >= 17);
+	    System.out.println("Status Dewasa : " + dewasa);
+    }
+}
